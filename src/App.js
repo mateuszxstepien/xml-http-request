782455902857e9cdcaa1469e4c2faa1e1a242487
mateuszxstepien <mateuszxstepien@gmail.com>
2023-03-07 12:15:34 +0100

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    users: [],
+  };
+
+  componentDidMount() {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://jsonplaceholder.typicode.com/users", true);
+    xhr.onload = () => {
+      console.log(xhr.status);
+      if (xhr.status === 200) {
+        const users = JSON.parse(xhr.response);
+        console.log(typeof users);
+        this.setState({
+          users,
+        });
+      }
+      console.log(typeof xhr.response);
+    };
+    // xhr.addEventListener("load", () => {
+    //   console.log(xhr.status);
+    //   if (xhr.status === 200) {
+    //     const users = JSON.parse(xhr.response);
+    //     console.log(typeof users);
+    //     this.setState({
+    //       users,
+    //     });
+    //   }
+    //   console.log(typeof xhr.response);
+    // });
+    xhr.send(null);
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.users.map((user) => (
+          <div key={user.id}>
+            {user.name} : {user.email}
+          </div>
+        ))}
+      </>
+    );
+  }
 }
 
 export default App;
